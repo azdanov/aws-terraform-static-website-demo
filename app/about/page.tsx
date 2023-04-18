1d41632f12745e17b0de2329a179770e5dcda13f
Anton Ždanov <anton@azdanov.dev>
2023-04-18 09:48:35 +0300

    add demo static app

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,21 @@
 import Link from "next/link";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={inter.className}>
       <section className="bg-white">
         <div className="layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center">
-          <h1 className="text-xl">AWS Terraform Static Website Demo</h1>
-          <p className="mt-2 text-sm text-gray-800">
-            This is a demo of a static website deployed to AWS using Terraform.
-          </p>
+          <h1 className="text-xl">About</h1>
+          <p className="mt-2 text-sm text-gray-800">About page.</p>
 
           <nav>
             <ul className="flex flex-col mt-4 space-y-2">
               <li>
-                <Link className="underline" href="/about">
-                  About
+                <Link className="underline" href="/">
+                  Home
                 </Link>
               </li>
             </ul>
